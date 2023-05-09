309ef489336ef863d23a2fb591fda60028a1ee85
Ahmed Soliman <ahmed-3010@hotmail.com>
2023-05-09 22:27:23 +0400

    VPC

--- main.tf
+++ main.tf
@@ -51,13 +51,14 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
 
 # resource "aws_security_group" "blog" {
 #   name = "blog"
